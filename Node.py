5df8741d5b8169ce5888610edf503dd165fbb9dd
wxiea <43360560+wxiea@users.noreply.github.com>
2018-11-18 13:53:07 -0700

    Add files via upload

---
+++ Node.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+8
+
+"""
+
+class Node:
+    def __init__(self, key, embedding, Next):
+        self.key = key
+        self.next = Next
+        self.embedding = embedding
---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import math
+import time
+from Node import Node
+
+def main():
+
+
+main()
